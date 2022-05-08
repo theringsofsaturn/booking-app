cbfa951f3f115d1983577add2a67945cf9355cfb
theringsofsaturn <emiliankasemi@gmail.com>
2022-05-09 01:35:31 +0200

    navbar with styles

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
+import { Navbar } from "../../components/Navbar";
+import "./home.css";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  return <Navbar />;
+};
 
-export default Home
+export default Home;
