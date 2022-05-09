bd92bb891e3c222d02a172a5f11c864cd8fbec1d
theringsofsaturn <emiliankasemi@gmail.com>
2022-05-09 02:35:34 +0200

    styles for header items

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,7 @@
 import {
   faBed,
   faCar,
+  faLandmark,
   faPlane,
   faTaxi,
 } from "@fortawesome/free-solid-svg-icons";
@@ -10,26 +11,28 @@ import "./header.css";
 const Header = () => {
   return (
     <div className="header">
-      <div className="headerList">
-        <div className="headerListItems">
-          <FontAwesomeIcon icon={faBed} />
-          <span>Bedrooms</span>
-        </div>
-        <div className="headerListItems">
-          <FontAwesomeIcon icon={faPlane} />
-          <span>Flights</span>
-        </div>
-        <div className="headerListItems">
-          <FontAwesomeIcon icon={faCar} />
-          <span>Car Rentals</span>
-        </div>
-        <div className="headerListItems">
-          <FontAwesomeIcon icon={faTaxi} />
-          <span>Airport Taxis</span>
-        </div>
-        <div className="headerListItems">
-          <FontAwesomeIcon icon={faBed} />
-          <span>Atractions</span>
+      <div className="headerContainer">
+        <div className="headerList">
+          <div className="headerListItems active">
+            <FontAwesomeIcon icon={faBed} />
+            <span>Bedrooms</span>
+          </div>
+          <div className="headerListItems">
+            <FontAwesomeIcon icon={faPlane} />
+            <span>Flights</span>
+          </div>
+          <div className="headerListItems">
+            <FontAwesomeIcon icon={faCar} />
+            <span>Car Rentals</span>
+          </div>
+          <div className="headerListItems">
+            <FontAwesomeIcon icon={faTaxi} />
+            <span>Airport Taxis</span>
+          </div>
+          <div className="headerListItems">
+            <FontAwesomeIcon icon={faLandmark} />
+            <span>Atractions</span>
+          </div>
         </div>
       </div>
     </div>
