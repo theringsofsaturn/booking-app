4d1de62260aae9793d9a9eda27b01151c99077ee
theringsofsaturn <emiliankasemi@gmail.com>
2022-05-15 02:23:14 +0200

    options for booking and styles

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -24,6 +24,9 @@ const Header = () => {
       key: "selection",
     },
   ]);
+
+  const [openOptions, setOpenOptions] = useState(false);
+  const [options, setOptions] = useState({ adult: 1, children: 0, room: 1 });
   return (
     <div className="header">
       <div className="headerContainer">
@@ -86,7 +89,38 @@ const Header = () => {
           </div>
           <div className="headerSearchItem">
             <FontAwesomeIcon icon={faPerson} className="headerIcons" />
-            <span className="headerSearchText">2 adults 2 children 1 room</span>
+            <span className="headerSearchText">
+              {`${options.adult} adult - ${options.children} children -
+              ${options.room} room `}
+            </span>
+            <div className="options">
+              <div className="optionItem">
+                <span className="optionText">Adult</span>
+                <div className="optionCounter">
+                  <button className="optionCounterButton">-</button>
+                  <span className="optionCounterNumber">1</span>
+                  <button className="optionCounterButton">+</button>
+                </div>
+              </div>
+
+              <div className="optionItem">
+                <span className="optionText">Children</span>
+                <div className="optionCounter">
+                  <button className="optionCounterButton">-</button>
+                  <span className="optionCounterNumber">0</span>
+                  <button className="optionCounterButton">+</button>
+                </div>
+              </div>
+
+              <div className="optionItem">
+                <span className="optionText">Room</span>
+                <div className="optionCounter">
+                  <button className="optionCounterButton">-</button>
+                  <span className="optionCounterNumber">1</span>
+                  <button className="optionCounterButton">+</button>
+                </div>
+              </div>
+            </div>
           </div>
           <div className="headerSearchItem">
             <button className="headerButton">Search</button>
